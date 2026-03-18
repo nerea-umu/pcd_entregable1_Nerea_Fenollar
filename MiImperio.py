5413b5913635c40456edb0baae556970e518e7b1
nerea.umu <nerea.fenollarp@um.es>
2026-03-18 21:24:52 +0100

    Culminando el código

--- MiImperio.py
+++ MiImperio.py
@@ -17,13 +17,11 @@ class StockInsuficienteError(ErrorImperio):
     """Se lanza cuando no hay suficientes unidades de un repuesto."""
     pass
 
-class ClaveIncorrectaError(ErrorImperio):
-    """Se lanza cuando la clave cifrada de la nave no coincide."""
-    pass
-
 class ObjetoErroneo(ErrorImperio):
     """Se lanza cuando un objeto proporcionado no es válido para el método."""
     pass
+
+
 
 
 class Clase(Enum):
@@ -50,45 +48,54 @@ class Unidad_Combate(ABC):
         self.clave_cif = clave_cif
 
     def __str__(self):
-        return f'UNIDAD DE COMBARE: \nID Combate: {self.id_combate} \nClave CIF: {self.clave_cif}'
+        return f'Unidad de combate: {self.id_combate} \nClave CIF: {self.clave_cif}\n'
 
-    def validar_clave(self, clave_cif):
-        if self.clave_cif != clave_cif:
-            raise ClaveIncorrectaError('La clave cifrada proporcionada no coincide con la clave de la nave.')
+
+
+
+
 
 
 class Nave(Unidad_Combate):
-    def __init__(self, nombre : str, piezas : list, id_combate : str, clave_cif : int):
+    def __init__(self, id_combate : str, clave_cif : int, nombre : str, piezas : list = None):
         super().__init__(id_combate, clave_cif)
         self.nombre = nombre
         self.piezas = piezas if piezas else []
 
+
     def __str__(self):
-        return super().__str__() + f'Nave: {self.nombre} \nPiezas: {len(self.piezas)} \nID Combate: {self.id_combate} \nClave CIF: {self.clave_cif}'
+        return super().__str__() + f'Nave: {self.nombre} \nPiezas: {len(self.piezas)}\n'
     
-    def solicitar_repuesto(self, almacen, repuesto, clave_cif):
-        if repuesto in self.piezas:
-            raise PiezaYaIncluida(f'La nave ya tiene el repuesto "{repuesto.nombre}".')
+
+
+    def solicitar_repuesto(self, almacen, repuesto):
         if not isinstance(repuesto, Repuesto):
             raise ObjetoErroneo('El objeto proporcionado no es un repuesto válido. Debe ser una instancia de la clase Repuesto.')
-        self.validar_clave(clave_cif)
-        almacen.eliminar_repuesto(repuesto.nombre, 1)
+
+        if repuesto in self.piezas:
+            raise PiezaYaIncluida(f'La nave ya tiene el repuesto "{repuesto.nombre}".')
+        
+        almacen.eliminar_repuesto(repuesto, 1)
         self.piezas.append(repuesto)
     
+
     def ver_piezas(self):
         print(f'Piezas de la nave {self.nombre}:')
         for pieza in self.piezas:
             print(pieza)
+        print()
+
+
 
 
 class Estacion_espacial(Propiedades, Nave):
     def __init__(self, nombre : str, piezas : list, id_combate : str, clave_cif : int, tripulacion : int, pasaje : int, ubicacion : Ubicacion):
         Propiedades.__init__(self, tripulacion, pasaje)
-        Nave.__init__(self, nombre, piezas, id_combate, clave_cif)
+        Nave.__init__(self, id_combate, clave_cif, nombre, piezas)
         self.ubicacion = ubicacion
 
     def __str__(self):
-        return f'Estacion Espacial: {self.nombre} \nPiezas: {len(self.piezas)} \nTripulación: {self.tripulacion} \nPasaje: {self.pasaje} \nUbicación: {self.ubicacion.name}'
+        return f'Tipo de unidad: Estación Espacial\n' + super().__str__() + f'\nTripulación: {self.tripulacion} \nPasaje: {self.pasaje} \nUbicación: {self.ubicacion.name}\n'
     
     def get_ubicacion(self):
         return self.ubicacion
@@ -103,11 +110,11 @@ class Estacion_espacial(Propiedades, Nave):
 class Nave_estelar(Propiedades, Nave):
     def __init__(self, nombre : str, piezas : list, id_combate : str, clave_cif : int, tripulacion : int, pasaje : int, clase : Clase):
         Propiedades.__init__(self, tripulacion, pasaje)
-        Nave.__init__(self, nombre, piezas, id_combate, clave_cif)
+        Nave.__init__(self, id_combate, clave_cif, nombre, piezas)
         self.clase = clase
     
     def __str__(self):
-        return f'Nave Estelar: {self.nombre} \nPiezas: {len(self.piezas)} \nTripulación: {self.tripulacion} \nPasaje: {self.pasaje} \nClase: {self.clase.name}'
+        return f'Tipo de unidad: Nave Estelar\n' + super().__str__() + f'\nTripulación: {self.tripulacion} \nPasaje: {self.pasaje} \nClase: {self.clase.name}\n'
     
     def get_clase(self):
         return self.clase
@@ -121,11 +128,11 @@ class Nave_estelar(Propiedades, Nave):
 
 class Caza(Nave):
     def __init__(self, nombre : str, piezas : list, id_combate : str, clave_cif : int, dotacion : int):
-        Nave.__init__(self, nombre, piezas, id_combate, clave_cif)
+        Nave.__init__(self, id_combate, clave_cif, nombre, piezas)
         self.dotacion = dotacion
     
     def __str__(self):
-        return f'Caza: {self.nombre} \nPiezas: {len(self.piezas)} \nDotación: {self.dotacion}'
+        return f'Tipo de unidad: Caza\n' + super().__str__() + f'Dotación: {self.dotacion}\n'
     
     def get_dotacion(self):
         return self.dotacion
@@ -142,12 +149,15 @@ class Repuesto:
         self.precio = precio
 
     def __str__(self):
-        return f'Repuesto: {self.nombre} \nCantidad: {self.__cantidad} \nProveedor: {self.proveedor} \nPrecio: ${self.precio:.2f}'
+        return f'Repuesto: {self.nombre} \nCantidad: {self.__cantidad} \nProveedor: {self.proveedor} \nPrecio: ${self.precio:.2f}\n'
     
     def actualiza_stock(self, unidades : int):
         if self.__cantidad + unidades < 0:
             raise StockInsuficienteError(f'No se puede reducir el stock del repuesto "{self.nombre}". Stock actual: {self.__cantidad}')
         self.__cantidad += unidades
+
+    def get_nombre(self):
+        return self.nombre
     
     def get_cantidad(self):
         return self.__cantidad
@@ -166,48 +176,53 @@ class Repuesto:
 
 
 class Almacen:
-    def __init__(self, nombre : str, ubicacion : str):
+    def __init__(self, nombre : str, ubicacion : str, catalogo : list = None):
         self.nombre = nombre
         self.ubicacion = ubicacion
-        self.catalogo = {}
+        self.catalogo = catalogo if catalogo else []
     
     def __str__(self):
-        return f'Almacén: {self.nombre} \nUbicación: {self.ubicacion} \nCatálogo: {len(self.catalogo)} repuestos'
+        return f'Almacén: {self.nombre} \nUbicación: {self.ubicacion} \nCatálogo: {len(self.catalogo)} repuestos\n'
     
+    def buscar_repuesto(self, nombre_pieza : str):
+        for repuesto in self.catalogo:
+            if repuesto.get_nombre() == nombre_pieza:
+                return repuesto
+        raise PiezaNoEncontradaError(f'El repuesto "{nombre_pieza}" no se encuentra en el catálogo.')
+    
+
     def ver_catalogo(self):
         print(f'Catálogo del almacén {self.nombre}:')
-        for repuesto, cantidad in self.catalogo.items():
-            print(f'-{repuesto} | Cantidad: {cantidad}')
+        for repuesto in self.catalogo:
+            print(f'-{repuesto}')
+        print('\n')
     
+
+
     def agregar_repuesto(self, repuesto : Repuesto):
         if not isinstance(repuesto, Repuesto):
             raise ObjetoErroneo('El objeto proporcionado no es un repuesto válido. Debe ser una instancia de la clase Repuesto.')
-        if repuesto in self.catalogo.keys():
-            self.catalogo[repuesto] += repuesto.get_cantidad()
+        if repuesto in self.catalogo:
+            raise PiezaYaIncluida(f'El repuesto "{repuesto.nombre}" ya existe en el catálogo.')
         else:
-            self.catalogo[repuesto] = repuesto.get_cantidad()
+            self.catalogo.append(repuesto)
     
-    def eliminar_repuesto(self, nombre_pieza : str, cantidad : int):
-        repuesto = self.buscar_repuesto(nombre_pieza)
-        if repuesto not in self.catalogo.keys(): 
-            raise PiezaNoEncontradaError(f'El repuesto "{nombre_pieza}" no se encuentra en el catálogo.')
+    def eliminar_repuesto(self, repuesto : Repuesto, cantidad : int):
+        if repuesto not in self.catalogo: 
+            raise PiezaNoEncontradaError(f'El repuesto "{repuesto.nombre}" no se encuentra en el catálogo.')
+        
         repuesto.actualiza_stock(-cantidad)
+
         if repuesto.get_cantidad() == 0:
-            del self.catalogo[repuesto]
-        else:
-            self.catalogo[repuesto] = repuesto.get_cantidad()
+            self.catalogo.remove(repuesto)
+
         return repuesto
     
-    def listar_repuestos(self):
-        print(f'Catálogo de repuestos de {self.nombre}:')
-        for r in self.catalogo:
-            print(r)
+    def eliminar_almacen(self):
+        self.catalogo.clear()
+        print(f'El almacén "{self.nombre}" ha sido eliminado.')
     
-    def buscar_repuesto(self, nombre : str):
-        for repuesto in self.catalogo.keys():
-            if repuesto.nombre == nombre:
-                return repuesto
-        return None
+
     
 
 class MiImperio():
@@ -218,17 +233,23 @@ class MiImperio():
         self.repuestos = []
     
     def __str__(self):
-        return f'Mi Imperio: {self.nombre} \nNaves: {len(self.naves)} \nAlmacenes: {len(self.almacenes)} \nRepuestos: {len(self.repuestos)}'
+        return f'Mi Imperio: {self.nombre} \nNaves: {len(self.naves)} \nAlmacenes: {len(self.almacenes)} \nRepuestos: {len(self.repuestos)}\n'
     
     def agregar_nave(self, nave : Nave):
         if not isinstance(nave, Nave):
             raise ObjetoErroneo('El objeto proporcionado no es una nave válida. Debe ser una instancia de la clase Nave.')
-        self.naves.append(nave)
+        if nave in self.naves:
+            raise PiezaYaIncluida(f'La nave "{nave.nombre}" ya existe en el imperio.')
+        else:
+            self.naves.append(nave)
     
     def eliminar_nave(self, nave : Nave):
         if not isinstance(nave, Nave):
             raise ObjetoErroneo('El objeto proporcionado no es una nave válida. Debe ser una instancia de la clase Nave.')
-        self.naves.remove(nave)
+        if nave not in self.naves:
+            raise PiezaNoEncontradaError(f'La nave "{nave.nombre}" no se encuentra en el imperio.')
+        else:
+            self.naves.remove(nave)
     
     def get_nave(self, nombre : str):
         for nave in self.naves:
@@ -242,15 +263,26 @@ class MiImperio():
             print(f'-{nave}')
         print()
     
+
+
     def agregar_almacen(self, almacen : Almacen):
         if not isinstance(almacen, Almacen):
             raise ObjetoErroneo('El objeto proporcionado no es un almacén válido. Debe ser una instancia de la clase Almacen.')
-        self.almacenes.append(almacen)
+        if almacen in self.almacenes:
+            raise PiezaYaIncluida(f'El almacén "{almacen.nombre}" ya existe en el imperio.')
+        else:
+            self.almacenes.append(almacen)
     
     def eliminar_almacen(self, almacen : Almacen):
         if not isinstance(almacen, Almacen):
             raise ObjetoErroneo('El objeto proporcionado no es un almacén válido. Debe ser una instancia de la clase Almacen.')
-        self.almacenes.remove(almacen)
+        if almacen not in self.almacenes:
+            raise PiezaNoEncontradaError(f'El almacén "{almacen.nombre}" no se encuentra en el imperio.')
+        else:
+            for almacen in self.almacenes:
+                if almacen.nombre == almacen.nombre:
+                    almacen.eliminar_almacen()
+                    self.almacenes.remove(almacen)
     
     def get_almacen(self, nombre : str):
         for almacen in self.almacenes:
@@ -264,6 +296,9 @@ class MiImperio():
             print(f'-{almacen}')
         print()
     
+
+
+
     def comprar_repuesto(self, repuesto : Repuesto):
         if not isinstance(repuesto, Repuesto):
             raise ObjetoErroneo('El objeto proporcionado no es un repuesto válido. Debe ser una instancia de la clase Repuesto.')
@@ -279,105 +314,137 @@ class MiImperio():
         for repuesto in self.repuestos:
             print(f'-{repuesto}')
         print()
-    
-    def buscar_repuesto(self, nombre : str):
-        for repuesto in self.repuestos:
-            if repuesto.nombre == nombre:
-                return repuesto
-        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
     try:
-        # 1. Creamos un almacén con algo de stock
-        almacen1 = Almacen("Depósito Imperial", "Cerca de la Estrella de la Muerte")
-        motor1 = Repuesto("Motor T-14", 2, "Chatarrerías Watto", 1500.0)
-        almacen1.agregar_repuesto(motor1)
+        print("=== PROTOCOLO DE PRUEBAS DEL SISTEMA IMPERIAL ===\n")
 
-        # 2. Creamos un Caza (Recuerda ajustar el constructor según tu Nave)
-        caza1 = Caza("CAZA Advanced", [], 'C-001', 5578, 1)
-        # Nota: asegúrate de que Caza pase el ID y Clave a Nave
-                
-        # 3. Intentamos pedir un repuesto con CLAVE INCORRECTA
-        print(almacen1)
-        try:
-            caza1.solicitar_repuesto(almacen1, motor1, 1500)
-            print(caza1)
-            almacen1.listar_repuestos()
-        except ErrorImperio as e:
-            print(f"Error: {e}")
+    
+        # 1. PRUEBA DE LA CLASE: Repuesto
+        print("-------------Probando repuestos:-------------")
+        r_motor = Repuesto("Motor Iónico S-1", 5, "Sienar Fleet Systems", 2500.0)
+        r_laser = Repuesto("Cañón Láser pesado", 2, "Kuat Drive Yards", 1200.0)
+        print(r_motor)
+        print(r_laser.get_proveedor())
+        r_lanzallamas = Repuesto("Lanzallamas FireBlast-9", 3, "Sienar Fleet Systems", 10000.0)
+        r_rueda = Repuesto("Rueda de repuesto", 7, "Kuat Drive Yards", 700.0)
+        r_motor2 = Repuesto("Motor Sónico S-8", 4, "Sienar Fleet Systems", 4500.0)
+        r_pantalla = Repuesto("Pantalla táctil P-12", 2, "Kuat Drive Yards", 1200.0)
+
+        # 2. PRUEBA DE LA CLASE: Almacen
+        print("\n-------------Probando almacenes:-------------")
+        almacen_central = Almacen("Almacén de Coruscant", "Sector 4")
+        almacen_central.agregar_repuesto(r_motor)
+        almacen_central.agregar_repuesto(r_laser)
+        print(almacen_central)
+        almacen_central.ver_catalogo()
+        almacen_jhonson = Almacen("Almacén de Jhonson", "Sector 7")
+        almacen_jhonson.agregar_repuesto(r_lanzallamas)
+        almacen_jhonson.agregar_repuesto(r_rueda)
+        almacen_jhonson.agregar_repuesto(r_motor2)
+        almacen_jhonson.agregar_repuesto(r_pantalla)
+        print(almacen_jhonson)
+        almacen_jhonson.ver_catalogo()
 
 
+        # 3. PRUEBA DE LA CLASE: Caza (Herencia de Nave)
+        print("\n-------------Probando cazas:-------------")
+        tie_vader = Caza("TIE Advanced x1", [], "VADER-01", 1138, 1)
+        print(tie_vader)
+        tie_vader.solicitar_repuesto(almacen_central, r_motor)
+        almacen_central.ver_catalogo()
+        tie_vader.solicitar_repuesto(almacen_jhonson, r_rueda)
+        print(tie_vader)
+        tie_vader.ver_piezas()
 
-        try:
-        # 4. Intentamos pedir un repuesto con CLAVE CORRECTA
-        # Suponiendo que la clave es 1500
-            caza1.solicitar_repuesto(almacen1, motor1, 5578)
-            print(caza1)
-            almacen1.listar_repuestos()
-        except ErrorImperio as e:
-            print(f"Error: {e}")
-            
+
+        # 4. PRUEBA DE LA CLASE: Nave_estelar (Herencia Múltiple y Enum Clase)
+        print("\n-------------Probando Nave Estelar:-------------")
+        ejecutor1 = Nave_estelar("Ejecutor-001", [], "EXEC-001", 9003, 50000, 38000, Clase.EJECUTOR)
+        print(ejecutor1)
+        print(ejecutor1.get_clase().name)
+        ejecutor1.solicitar_repuesto(almacen_jhonson, r_pantalla)
+        ejecutor1.ver_piezas()
 
 
-           
-        print("--- INICIANDO PROTOCOLO DE PRUEBAS IMPERIALES ---\n")
+        # 5. PRUEBA DE LA CLASE: Estacion_espacial (Herencia Múltiple y Enum Ubicación)
+        print("\n-------------Probando Estación Espacial:-------------")
+        estrella_muerte = Estacion_espacial("Estrella de la Muerte", [], "DS-01", 4439, 250000, 1000000, Ubicacion.ENDOR)
+        estrella_muerte.solicitar_repuesto(almacen_central, r_laser)
+        estrella_muerte.solicitar_repuesto(almacen_jhonson, r_lanzallamas)
+        print(estrella_muerte)
+        print(estrella_muerte.get_ubicacion().name)
+        print(f'Capacidad total de la estación: {estrella_muerte.get_capacidad()} mandalorianos')
+
+
+        # 6. PRUEBA DE LA CLASE: MiImperio (Gestión de colecciones)
+        print("\n-------------Probando Imperio:-------------")
+        imperio = MiImperio("Imperio Galáctico")
+        imperio.agregar_nave(tie_vader)
+        imperio.agregar_nave(ejecutor1)
+        imperio.agregar_nave(estrella_muerte)
+        imperio.agregar_almacen(almacen_central)
+        imperio.agregar_almacen(almacen_jhonson)
+        imperio.listar_naves()
+        imperio.get_nave("TIE Advanced x1").ver_piezas()
+        imperio.listar_almacenes()
+
+
+        # 8. PRUEBA DE EXCEPCIONES:
+        print("\n-------------Probando Excepciones:-------------")
         
-        # 1. Configuración Inicial
-        almacen_vader = Almacen("Depósito Estrella de la Muerte", "Sector 0")
-        motor = Repuesto("Motor Iónico T-IE", 1, "Sienar Fleet", 5000.0)
-        almacen_vader.agregar_repuesto(motor)
-        
-        mi_caza = Caza("TIE Advanced x1", [], "VADER-001", 1138, 1)
-
-        # --- TRANSACCIÓN 1: Éxito ---
-        print("TRANSACCIÓN 1: Solicitud correcta")
+        # Error 1: Pieza ya incluida
         try:
-            mi_caza.solicitar_repuesto(almacen_vader, motor, 1138)
-        except ErrorImperio as e:
-            print(f"Error: {e}")
-        print("-" * 30)
+            tie_vader.solicitar_repuesto(almacen_central, r_motor)
+        except PiezaYaIncluida as e:
+            print(f"   [CAPTURA] Error esperado: {e}")
 
-        # --- TRANSACCIÓN 2: Error de Clave ---
-        print("TRANSACCIÓN 2: Clave de seguridad incorrecta")
+        # Error 2: Stock insuficiente
         try:
-            mi_caza.solicitar_repuesto(almacen_vader, motor, 9999)
-        except ErrorImperio as e:
-            print(f"CAPTURA EXITOSA: {e}")
-        print("-" * 30)
+            almacen_central.eliminar_repuesto(r_laser, 10) # Pedimos 10 pero solo hay 2
+        except StockInsuficienteError as e:
+            print(f"   [CAPTURA] Error esperado: {e}")
 
-        # --- TRANSACCIÓN 3: Error de Pieza Repetida ---
-        print("TRANSACCIÓN 3: Pieza ya instalada")
+        # Error 3: Objeto Erróneo
         try:
-            # Volvemos a pedir el motor que ya instalamos en la T1
-            mi_caza.solicitar_repuesto(almacen_vader, motor, 1138)
-        except ErrorImperio as e:
-            print(f"CAPTURA EXITOSA: {e}")
-        print("-" * 30)
+            almacen_central.agregar_repuesto("Esto no es una pieza")
+        except ObjetoErroneo as e:
+            print(f"   [CAPTURA] Error esperado: {e}")
 
-        # --- TRANSACCIÓN 4: Error de Stock ---
-        print("TRANSACCIÓN 4: Stock insuficiente")
-        try:
-            # Añadimos una pieza nueva pero solo 1 unidad
-            laser = Repuesto("Cañón Láser L-s9", 1, "Sienar", 1200.0)
-            almacen_vader.agregar_repuesto(laser)
-            
-            caza_aux = Caza("TIE Fighter", [], "AUX-99", 0000, 1)
-            caza_aux.solicitar_repuesto(almacen_vader, laser, 0000) # Se lleva el único que hay
-            
-            # Intentamos pedir otro
-            mi_caza.solicitar_repuesto(almacen_vader, laser, 1138)
-        except ErrorImperio as e:
-            print(f"CAPTURA EXITOSA: {e}")
-        print("-" * 30)
+        print("\n=== PRUEBAS FINALIZADAS CON ÉXITO ===")
 
-        # --- RESULTADO FINAL ---
-        print("\n--- RESUMEN DE ESTADO ---")
-        print(mi_caza)
-        mi_caza.ver_piezas()
-        almacen_vader.ver_catalogo()
-
-    except ErrorImperio as e:
-        print(f"Error en la operación: {e}")
+    except Exception as e:
+        print(f"\n[ERROR CRÍTICO] Se ha detenido la prueba por: {e}")
+        import traceback
+        traceback.print_exc()
